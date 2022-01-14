4bfc08b50e338515d404365cedc0e5afd8b6a1d4
Benjamin <b.oechsle@web.de>
2022-01-14 17:16:14 +0100

    input transactions now know their address and value

--- src/callbacks/mongo.rs
+++ src/callbacks/mongo.rs
@@ -1,4 +1,4 @@
-use std::slice::SliceIndex;
+use std::{collections::HashMap, slice::SliceIndex};
 
 use mongodb::{
     bson::{doc, Document},
@@ -77,9 +77,12 @@ impl Callback for Mongo {
 
         let block_hash = utils::arr_to_hex_swapped(&block.header.hash);
         let mut transactions: Vec<Document> = Vec::new();
+        let mut tx_map = HashMap::new();
 
         for tx in &block.txs {
-            transactions.push(tx.as_doc(&block_hash, &self.tx_collection))
+            transactions.push(tx.as_doc(&block_hash, &self.tx_collection, &tx_map));
+            let (tx_hash, output_map) = tx.as_map_tuple();
+            tx_map.insert(tx_hash, output_map);
         }
         self.tx_collection.insert_many(transactions, None)?;
         self.tx_count += block.tx_count.value;
@@ -119,15 +122,20 @@ impl Block {
 
 impl Hashed<EvaluatedTx> {
     #[inline]
-    fn as_doc(&self, block_hash: &str, collection: &Collection<Document>) -> Document {
+    fn as_doc(
+        &self,
+        block_hash: &str,
+        collection: &Collection<Document>,
+        tx_map: &HashMap<String, HashMap<i32, (i64, String)>>,
+    ) -> Document {
         let mut inputs: Vec<Document> = Vec::new();
         let mut outputs: Vec<Document> = Vec::new();
         let txid_str = &utils::arr_to_hex_swapped(&self.hash);
-        for (i, input) in self.value.inputs.iter().enumerate() {
-            inputs.push(input.as_doc(&txid_str, i as i32, collection))
-        }
         for (i, output) in self.value.outputs.iter().enumerate() {
             outputs.push(output.as_doc(&txid_str, i as i32))
+        }
+        for (i, input) in self.value.inputs.iter().enumerate() {
+            inputs.push(input.as_doc(&txid_str, i as i32, collection, tx_map))
         }
         doc! {
                     "txHash": &txid_str,
@@ -140,28 +148,71 @@ impl Hashed<EvaluatedTx> {
                     "txOutputs": outputs
         }
     }
+
+    #[inline]
+    fn as_map_tuple(&self) -> (String, HashMap<i32, (i64, String)>) {
+        let mut output_map = HashMap::new();
+        let txid_str = &utils::arr_to_hex_swapped(&self.hash);
+        for (i, output) in self.value.outputs.iter().enumerate() {
+            output_map.extend(output.as_map(i as i32));
+        }
+        return (txid_str.to_string(), output_map);
+    }
 }
 
 impl TxInput {
     #[inline]
-    fn as_doc(&self, txid: &str, index: i32, collection: &Collection<Document>) -> Document {
+    fn as_doc(
+        &self,
+        txid: &str,
+        index: i32,
+        collection: &Collection<Document>,
+        tx_map: &HashMap<String, HashMap<i32, (i64, String)>>,
+    ) -> Document {
         let hash_prev_out = &utils::arr_to_hex_swapped(&self.outpoint.txid);
         let index_prev_out = &self.outpoint.index;
-        // let prev_out_tx = collection.find_one(doc! {"txHash": hash_prev_out}, None);
-        //         match prev_out_tx {
-        //             Ok(Some(data)) => println!("res: {:?}", data.get_array("txOutputs")[index_prev_out]),
-        //             Ok(None) => println!("einfach nein"),
-        //             Err(e) => println!("einfach nein {:?}", e),
-        //         };
-        //
-        // (@txid, @hashPrevOut, indexPrevOut, scriptSig, sequence)
+        let mut value = 0;
+        let mut address: String =
+            "0000000000000000000000000000000000000000000000000000000000000000".to_string();
+
+        // if the previous tx is no doge reward set value and address
+        if hash_prev_out != "0000000000000000000000000000000000000000000000000000000000000000" {
+            if let Some(output_map) = tx_map.get(hash_prev_out) {
+                if let Some((new_value, new_address)) = output_map.get(&(*index_prev_out as i32)) {
+                    value = *new_value as i64;
+                    address = new_address.to_string();
+                } else if let Some(prev_out_tx) = collection
+                    .find_one(doc! {"txHash": hash_prev_out}, None)
+                    .unwrap()
+                {
+                    let tx_outputs = &prev_out_tx.get_array("txOutputs").unwrap()
+                        [*index_prev_out as usize]
+                        .as_document()
+                        .unwrap();
+
+                    value = tx_outputs.get_i64("value").unwrap();
+
+                    address = tx_outputs.get_str("address").unwrap().to_string();
+                    // println!("Adress: {} Value: {}", address, value);
+                } else {
+                    println!(
+                        "No Transaction for the Input found for TX: {} with PrevOut: {}",
+                        txid, hash_prev_out
+                    );
+                };
+            }
+            // if previous out tx exists
+        }
+
         doc!(
             "txHash": &txid,
             "hashPrevOut": hash_prev_out,
             "indexPrevOut": index_prev_out,
             "indexIn": *&index,
             "scriptSig": &utils::arr_to_hex(&self.script_sig),
-            "sequenceNumber": &self.seq_no
+            "sequenceNumber": &self.seq_no,
+            "value": value,
+            "address": address
         )
     }
 }
@@ -185,5 +236,17 @@ impl EvaluatedTxOut {
             "scriptPubKey": &utils::arr_to_hex(&self.out.script_pubkey),
             "address": &address
         )
+    }
+
+    #[inline]
+    fn as_map(&self, index: i32) -> HashMap<i32, (i64, String)> {
+        let address = match self.script.address.clone() {
+            Some(address) => address,
+            None => String::new(),
+        };
+        let mut map = HashMap::new();
+        // index, value, address
+        map.insert(index, (*&self.out.value as i64, address));
+        return map;
     }
 }
